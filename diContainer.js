d689799cb17c13892aee355101eede34ff7ac77c
venu-shastri <venu.shastri@gmail.com>
2019-05-24 10:22:16 +0530

    Update diContainer.js

--- diContainer.js
+++ diContainer.js
@@ -1,6 +1,24 @@
 "use strict";
 
-const fnArgs = require('parse-fn-args');
+//const fnArgs = require('parse-fn-args');
+var fnArgs= function (fn) {
+  var src = fn.toString()
+
+  // remove comments
+  src = src.replace(/(\/\*([\s\S]*?)\*\/|\/\/(.*)$)/mg, '')
+
+  var bi = src.indexOf('(')
+  var ai = src.indexOf('=>')
+
+  var args = ai > 0 && (ai < bi || bi < 0)
+    ? src.slice(0, ai)
+    : src.slice(bi + 1, src.indexOf(')'))
+
+  args = args.replace(/\s+/g, '')
+
+  return args ? args.split(',') : []
+};
+
 
 module.exports = () => {
   const dependencies = {};
